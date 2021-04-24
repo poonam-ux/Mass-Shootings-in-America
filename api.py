9430e2717d50036e8516006c6c11bc4944cacbec
poonam-ux <shaonam@gmail.com>
2021-04-24 14:27:46 -0700

    Add API routes

--- api.py
+++ api.py
@@ -16,5 +16,42 @@ def chart_view():
 def table_view():
     return render_template('TableView.html')
 
+@app.route('/data/<path:filename>')
+def custom_static(filename):
+    return send_from_directory('data', filename, as_attachment=False)
+
+
+@app.route('/query', methods=['GET','POST'])
+def query_selected_post():
+    reqFilter = request.args.get('requestType')
+    reqVal = request.args.get('requestValue')
+    csv_file = csv.reader(open('data/mass_shootings.csv', "r"), delimiter=",")
+    jsonData = {}
+    counter = 0
+    for row in csv_file:
+    #if current rows 2nd value is equal to input, print that row
+        if reqVal == 'ALL':
+            jsonData[counter] = row
+            counter +=1
+        if reqVal == row[18] and reqFilter == 'raceSelect':
+            jsonData[counter] = row
+            counter +=1
+
+        if reqVal == row[11] and reqFilter == 'ageSelect':
+            jsonData[counter] = row
+            counter +=1
+    return jsonify(result=jsonData)
+
+@app.route('/queryall', methods=['GET','POST'])
+def query_all_post():
+    csv_file = csv.reader(open('data/mass_shootings.csv', "r"), delimiter=",")
+    jsonData = {}
+    counter = 0
+    for row in csv_file:
+        jsonData[counter] = row
+        counter +=1
+       
+    return jsonify(result=jsonData)
+
 if __name__ == '__main__':
     app.run(debug=True)
